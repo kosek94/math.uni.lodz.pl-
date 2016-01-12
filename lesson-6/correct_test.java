937efa45554046b0e407a98e97274aba8478789e
Krzysztof Kosek <kosek94@o2.pl>
2016-01-12 20:55:38 +0100

    lesson-6

---
+++ lesson-6/correct_test.java
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head profile="http://selenium-ide.openqa.org/profiles/test-case">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="selenium.base" href="http://localhost:8080/" />
+<title>correct_test</title>
+</head>
+<body>
+<table cellpadding="1" cellspacing="1" border="1">
+<thead>
+<tr><td rowspan="1" colspan="3">correct_test</td></tr>
+</thead><tbody>
+<tr>
+	<td>open</td>
+	<td>/registrationform/</td>
+	<td></td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>name=firstName</td>
+	<td>qqq</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>name=lastName</td>
+	<td>www</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>name=email</td>
+	<td>as@asss.s</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>name=repeatEmail</td>
+	<td>as@asss.s</td>
+</tr>
+<tr>
+	<td>click</td>
+	<td>css=button.gwt-Button</td>
+	<td></td>
+</tr>
+<tr>
+	<td>click</td>
+	<td>id=gwt-uid-25</td>
+	<td></td>
+</tr>
+<tr>
+	<td>click</td>
+	<td>id=gwt-uid-26</td>
+	<td></td>
+</tr>
+<tr>
+	<td>click</td>
+	<td>css=table.form-buttons &gt; tbody &gt; tr &gt; td &gt; button.gwt-Button</td>
+	<td></td>
+</tr>
+<tr>
+	<td>selectWindow</td>
+	<td>null</td>
+	<td></td>
+</tr>
+<tr>
+	<td>assertText</td>
+	<td>css=div.GOFRIDJCEI</td>
+	<td>Congratulations! You've won:</td>
+</tr>
+
+</tbody></table>
+</body>
+</html>
